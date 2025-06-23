dc11551627cec47966cf0b8c92455d2542690636
paramesh <parameshdarshan1234@gmail.com>
2025-06-23 17:36:54 +0530

    Update controller path to /api/v2

--- src/main/java/com/javatechie/crud/example/controller/ProductController.java
+++ src/main/java/com/javatechie/crud/example/controller/ProductController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v2")
 @CrossOrigin(origins = "*")
 public class ProductController {
 
